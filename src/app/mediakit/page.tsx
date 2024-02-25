152b80a01a2f70188da1aa5049ad2343bd4bba69
marypztr <maryjose@99minutos.com>
2024-02-24 19:37:12 -0600

    fisrt commit

---
+++ src/app/mediakit/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function MediaKit() {
+  return (
+    <main className={styles.main}>
+        <h2>Media Kit</h2>
+    </main>
+  );
+}
---
+++ src/app/shop/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function Shop() {
+  return (
+    <main className={styles.main}>
+        <h2>Shop</h2>
+    </main>
+  );
+}
